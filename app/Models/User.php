34e6edba738c8fe3b740d6057dfc82089aef9b4c
andy <andy.liujin@foxmail.com>
2019-11-01 07:41:33 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,40 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    // 粉丝列表 (自己是明星 得出 粉丝列表)
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注人列表 （明星）（自己是粉丝 得出 明星列表）
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 某用户是否关注了 传参用户 （某用户的关注人列表中是否包含传参用户）
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
